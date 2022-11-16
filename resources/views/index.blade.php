e2f8120f89c6b060f1f6b92c233344cbe506003a
Giancarix117 <giancarlobetti2008@hotmail.com>
2022-11-16 11:38:23 +0100

    Home page actualized

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,12 @@
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
                 <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    Do you want to share your pain with the world?
+                    Everything Cyber Security related at the tip of your fingers. Tales from real field experts to your eyes.
                 </h1>
                 <a
                     href="/blog"
                     class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
-                    Read More
+                    Get CyberSecing!
                 </a>
             </div>
         </div>
@@ -18,20 +18,20 @@
 
     <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
         <div>
-            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
+            <img class ="object-fill" src="https://www.kaspersky.com/content/en-global/images/repository/isc/2017-images/What-is-Cyber-Security.jpg" width="700" alt="" height="700" >
         </div>
 
         <div class="m-auto sm:m-auto text-left w-4/5 block">
             <h2 class="text-3xl font-extrabold text-gray-600">
-                Struggling to be a better person?
+                Want to know more about Cyber Security?
             </h2>
 
             <p class="py-8 text-gray-500 text-s">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
+                You came to the right place
             </p>
 
             <p class="font-extrabold text-gray-600 text-s pb-9">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente magnam vero nostrum! Perferendis eos molestias porro vero. Vel alias.
+                Whether you are a mere peasant trying to learn more about CyberSecurity, or an absolute CyberChad with years of experience, this is a place for everyone to share their personal stories.
             </p>
 
             <a
@@ -44,20 +44,20 @@
 
     <div class="text-center p-15 bg-black text-white">
         <h2 class="text-2xl pb-5 text-l">
-            I'm an expert in...
+            Learn about
         </h2>
 
         <span class="font-extrabold block text-4xl py-1">
-            Ux Design
+            Penetration Testing
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Project Management
+            System Security
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Digital Strategy
+            Recent Hacks
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Backend Development
+            and much, much more!
         </span>
     </div>
 
